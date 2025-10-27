fc08832a085afa04f3413491e91f0534adc3ab1d
TabulateJarl8 <tabulatejarl8@gmail.com>
2025-10-27 00:21:51 -0400

    change demo to tty0

--- demo.c
+++ demo.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-// simple demo program that writes text to TTY1
+// simple demo program that writes text to TTY0
 // run with `cat a.out > /dev/exec`
 int main() {
-	FILE* f = fopen("/dev/pts/1", "w");
-	fprintf(f, "Hello world\n");
-	fclose(f);
+  FILE *f = fopen("/dev/pts/0", "w");
+  fprintf(f, "Hello world\n");
+  fclose(f);
 }
